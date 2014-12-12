2df6a133daaa865a7aefb54ffc6e4743086ff10b
Dennis Breen <dennis.breen@nform.com>
2014-12-12 12:00:59 -0600

    Initial setup

---
+++ public/js/main.js
@@ -0,0 +1,10 @@
+// Define the Knockout View Model
+var viewModel = {
+};
+
+// Run this block when the page is ready
+$(function(){
+
+	// Start the Knockout View Model
+	ko.applyBindings(viewModel); 
+});
